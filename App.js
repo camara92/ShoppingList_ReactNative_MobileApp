4201ab9c7c0a7936aa4ad1b6f889318b2ed0feb1
camara92 <88945535+camara92@users.noreply.github.com>
2022-07-08 00:36:36 +0200

    textinput btn, onchangetexxt , submitted and shows

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , 
+  TextInput,Button, 
+placeholder} from 'react-native';
+import React, {useState} from 'react';
+
 
 export default function App() {
+  const [product, setProduct] = useState('');
+  const [Myproducts, setMyProducts] = useState([]);
+  const inputHandler= (val)=>{
+    setProduct(val)
+  }
+  const submitHandler=()=>{
+    //console.log(product); 
+    // setMyProducts([...Myproducts, product])
+    setMyProducts( currentMyProduct => [...currentMyProduct, product])
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+      <TextInput 
+        style={styles.textInput}
+        placeholder="Saisir votre texte"
+        onChangeText={ inputHandler }
+        value={product}
+      />
+      <Button 
+        title='Valider'
+        onPress={submitHandler}
+       />
+      </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 40, 
+    paddingTop: 60
   },
+  inputContainer:{
+    flexDirection:"row", 
+
+  }, 
+  textInput:{
+    borderColor: "grey", 
+    borderWidth:1, padding: 5, 
+    paddingLeft: 9, 
+    fontSize:18, 
+    flexGrow:1, 
+    // flexgrowth :prend l'espace dispo
+
+
+
+  }
 });
